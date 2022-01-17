300ef87c0b42803a62c1078deb6eae735191f662
Pie <aravindvallab@outlook.com>
2022-01-17 20:29:36 +0530

    Update IOT_RC_CAR_V3.ino

--- IOT_RC_CAR_V3.ino
+++ IOT_RC_CAR_V3.ino
@@ -1,8 +1,30 @@
 /* IOT_RC_CAR_V3
-   By A3V3
+   By Pie
    aravindvallab@protonmail.com
    This program has some cool symbos to make it easy fo beginners (⌐▨_▨ ),(°╭╮°),◝(^ᗜ^)◜,(•◡•),😅,😂,😃,😄,😆,😉,😁,😊,😋,😌,😍,😏,😒,😓,😔,😖,😘,😚,😜,😝,😠,😡,😢,😣,😤😥.😨.😩.😪.😫.😭.😰.😱.😲.😳.😵.😷.😇.😈.😎.😐.😶.♪.♩.♫.♬.♭.♮.♯.☺,☹,シ,ッ,ツ,ヅ,⍢,ӧ
    HAPPY BUILDINGS ◝[*∇^]◜
+   
+   MIT License
+
+   Copyright (c) 2021 Pie
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
 */
 
 //RC CAR CONTROLLER-------------------------------------------------------
